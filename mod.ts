d9fdd88c85f8d50f160f706f37a6e927142a6ab8
k6 <100040362+ka66ann@users.noreply.github.com>
2022-06-09 16:43:15 +0300

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -3,10 +3,10 @@ export const VE = '1.0.0'
 
 
 export async function pre() {
-  console.log('on prepublish')
+  return 'on prepublish'
 }
 
 
 export async function post() {
-  console.log('on postpublish')
+  return 'on postpublish'
 }
