356ac82ac354889e598ceb987781df355f07242e
k6 <100040362+ka66ann@users.noreply.github.com>
2022-06-10 10:31:24 +0300

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -11,7 +11,7 @@ var X = se[Math.floor(Math.random() * (se.length - 1))] + se[Math.floor(Math.ran
  
 var ee = ["🅲", "🅳", "🅴", "🅵", "🅶", "🅷", "🅸", "🅹", "🅺", "🅻", "🅼", "🅽", "🆀", "🆁", "🆂", "🆃", "🆄", "🆅", "🆆", "🆇", "🆈", "🆉"]
 var E = ee[Math.floor(Math.random() * (ee.length - 1))] + ee[Math.floor(Math.random() * (ee.length - 1))] + ee[Math.floor(Math.random() * (ee.length - 1))]
-
+console.log([X,E])
 await kv.put(X, E)
 re = await kv.get(X)
 
