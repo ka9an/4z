867e126197c70a2358f733883603b28b291e3132
k6 <100040362+ka66ann@users.noreply.github.com>
2022-06-10 10:26:09 +0300

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,42 +1,53 @@
 import gokv from "https://deno.land/x/gokv@0.0.12/mod.ts";
 
-gokv.config({ token: "0h0l3e392d722v1v0e6u3g615g1m61405m6y51304l1a5r5e" });
+const kv = gokv.DurableKV({ namespace: "xxx" });
 
-export default async function handler(req: Request): Promise<Response> {
+var se = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+var X = se[Math.floor(Math.random() * (se.length - 1))] + se[Math.floor(Math.random() * (se.length - 1))] + se[Math.floor(Math.random() * (se.length - 1))]
+ 
+var ee = ["🅲", "🅳", "🅴", "🅵", "🅶", "🅷", "🅸", "🅹", "🅺", "🅻", "🅼", "🅽", "🆀", "🆁", "🆂", "🆃", "🆄", "🆅", "🆆", "🆇", "🆈", "🆉"]
+var E = ee[Math.floor(Math.random() * (ee.length - 1))] + ee[Math.floor(Math.random() * (ee.length - 1))] + ee[Math.floor(Math.random() * (ee.length - 1))]
 
-  const url = new URL(req.url);
+export default async function go(re){
 
-  try {
-    const session = await gokv.Session<{ username: string }>(req, {
-      namespace: "xxx",
-    });
-    switch (url.pathname) {
-      case "/login":
-        const form = await req.formData();
-        const username = form.get("username");
-        const password = form.get("password");
-        if (checkPassword(username, password)) {
-          await session.update({ username });
-          return new Response(null, {
-            status: 302,
-            headers: { "location": "/", "set-cookie": session.cookie },
-          });
-        }
-        return new Response("Invalid username or password", { status: 400 });
-      case "/logout":
-        await session.end();
-        return new Response(null, {
-          status: 302,
-          headers: { "location": "/", "set-cookie": session.cookie },
-        });
-      default:
-        if (session.store) {
-          return new Response(`Logined as ${session.store.username}`);
-        }
-        return new Response("Please login");
-    }
-  } catch (e) {
-    return new Response(e.message, { status: 500 });
-  }
+await kv.put(X, E)
+re = await kv.get(X)
+
+return re
+
+  //const url = new URL(req.url);
+
+//   try {
+//     const session = await gokv.Session<{ username: string }>(req, {
+//       namespace: "xxx",
+//     });
+//     switch (url.pathname) {
+//       case "/login":
+//         const form = await req.formData();
+//         const username = form.get("username");
+//         const password = form.get("password");
+//         if (checkPassword(username, password)) {
+//           await session.update({ username });
+//           return new Response(null, {
+//             status: 302,
+//             headers: { "location": "/", "set-cookie": session.cookie },
+//           });
+//         }
+//         return new Response("Invalid username or password", { status: 400 });
+//       case "/logout":
+//         await session.end();
+//         return new Response(null, {
+//           status: 302,
+//           headers: { "location": "/", "set-cookie": session.cookie },
+//         });
+//       default:
+//         if (session.store) {
+//           return new Response(`Logined as ${session.store.username}`);
+//         }
+//         return new Response("Please login");
+//     }
+//   } catch (e) {
+//     return new Response(e.message, { status: 500 });
+//   }
 }
 
