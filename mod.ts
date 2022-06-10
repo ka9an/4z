a5453fb9cb8c574ad7ef14ff5afd548d456b97f4
k6 <100040362+ka66ann@users.noreply.github.com>
2022-06-10 09:40:34 +0300

    Update mod.ts

--- mod.ts
+++ mod.ts
@@ -1,12 +1,42 @@
-export const VE = '1.0.0'
+import gokv from "https://deno.land/x/gokv@0.0.12/mod.ts";
 
+gokv.config({ token: "0h0l3e392d722v1v0e6u3g615g1m61405m6y51304l1a5r5e" });
 
+export default async function handler(req: Request): Promise<Response> {
 
-export async function pre() {
-  return 'on prepublish'
+  const url = new URL(req.url);
+
+  try {
+    const session = await gokv.Session<{ username: string }>(req, {
+      namespace: "xxx",
+    });
+    switch (url.pathname) {
+      case "/login":
+        const form = await req.formData();
+        const username = form.get("username");
+        const password = form.get("password");
+        if (checkPassword(username, password)) {
+          await session.update({ username });
+          return new Response(null, {
+            status: 302,
+            headers: { "location": "/", "set-cookie": session.cookie },
+          });
+        }
+        return new Response("Invalid username or password", { status: 400 });
+      case "/logout":
+        await session.end();
+        return new Response(null, {
+          status: 302,
+          headers: { "location": "/", "set-cookie": session.cookie },
+        });
+      default:
+        if (session.store) {
+          return new Response(`Logined as ${session.store.username}`);
+        }
+        return new Response("Please login");
+    }
+  } catch (e) {
+    return new Response(e.message, { status: 500 });
+  }
 }
 
-
-export async function post() {
-  return 'on postpublish'
-}
